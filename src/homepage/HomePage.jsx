599682b1a72da80a0dec8d1f7f0563d774725a60
AlexanderWiklow <a.wiklow@gmail.com>
2022-06-16 22:25:48 +0300

    initial commit

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -1,14 +1,31 @@
-function HomePage() {
-
-    const containerStyle = {
-        margin: "10% 20% 10% 20%",
-        backgroundColor: "lightblue",
-        height: "80vh"
+function LandingFrame() {
+    const style = {
+                "background-image": `url("images/wallpaper.jpeg")`,
+        "background-repeat": "no-repeat",
+        "background-size": "cover",
+        position: "absolute",
+        height: "100%",
+        width: "100%"
     }
 
-    return <div style={containerStyle}>
-        This is the home page
-    </div>
+    return <div style={style}></div>
+}
+
+function HomePage() {
+
+    // const containerStyle = {
+    //     margin: "10% 20% 10% 20%",
+    //     backgroundColor: "lightblue",
+    //     height: "80vh"
+    // }
+
+    // return <div style={containerStyle}>
+    //     This is the home page
+    // </div>
+
+    return <div>
+            <LandingFrame/>
+                </div>
 }
 
 export default HomePage
